004d0eb4862c62a706accb394cd23ea13a4a389d
viyoriya <viyoriya@gmail.com>
2020-06-02 13:55:24 +0100

    dex added

--- .config/dwm/dex.sh
+++ .config/dwm/dex.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
-DMENU='dmenu -i -l 20'
+#Custom Nord color
+DMENU_OPT=" -i -nb #17191C -nf #5E81AC -sb #4C566A -sf #D8DEE9 -fn Iosevka:style=Regular:pixelsize=12"    
+DMENU="dmenu -l 10 -p Dir $DMENU_OPT"
 SLASH="/"
+#Change your terminal and Editor
+TERM="st -e "
 EDITOR="nvim"
+#Change it to your application
+IMG="feh"
+PDF="zathura"
+TAR="tar -xvzf"
+ZIP="unzip"
 
+#Add more file extension(s)
+openFile(){
+    case "$1" in
+        *.tar.gz)  $TAR "$1" &>/dev/null & disown;;
+        *.tgz)     $TAR "$1" &>/dev/null & disown;;
+        *.zip)     $ZIP "$1" &>/dev/null & disown;;
+        *.jpg)     $IMG "$1" &>/dev/null & disown;;
+        *.JPG)     $IMG "$1" &>/dev/null & disown;;
+        *.jpeg)    $IMG "$1" &>/dev/null & disown;;
+        *.JPEG)    $IMG "$1" &>/dev/null & disown;;
+        *.gif)     $IMG "$1" &>/dev/null & disown;;
+        *.GIF)     $IMG "$1" &>/dev/null & disown;;
+        *.png)     $IMG "$1" &>/dev/null & disown;;
+        *.PNG)     $IMG "$1" &>/dev/null & disown;;
+        *.pdf)     $PDF "$1" &>/dev/null & disown;;
+        *.PDF)     $PDF "$1" &>/dev/null & disown;;
+        *)         $TERM $EDITOR "$1" &>/dev/null & disown;;
+    esac 
+}
+# type q to exit
 callAgain(){
-    
-    if [[ $1 == "" ]]; then
-        echo "Please enter dir name as argument"
-        exit
-    fi
-
-    
     if [[ $1 == *"/q"* ]]; then
         exit
     fi
 
-    choice=$(ls ${1} | $DMENU -p "Dir:")
+    choice=$(ls -a ${1} | $DMENU )
     if [[ -f $1$SLASH$choice ]] 
     then
-        $EDITOR $1$SLASH$choice
+         openFile "$1$SLASH$choice"
     else
         callAgain $1$SLASH$choice
     fi
 }
 
-callAgain $1
-
-
+callAgain "$HOME/"
--- .config/dwm/dwm.c
+++ .config/dwm/dwm.c
@@ -170,6 +170,7 @@ static void focus(Client *c);
 static void focusin(XEvent *e);
 static void focusmon(const Arg *arg);
 static void focusstack(const Arg *arg);
+static Atom getatomprop(Client *c, Atom prop);
 static int getrootptr(int *x, int *y);
 static long getstate(Window w);
 static int gettextprop(Window w, Atom atom, char *text, unsigned int size);
@@ -716,7 +717,7 @@ dirtomon(int dir)
 void
 drawbar(Monitor *m)
 {
-	int x, w, sw = 0;
+	int x, w, tw = 0;
 	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
@@ -725,8 +726,8 @@ drawbar(Monitor *m)
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
-		sw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
-		drw_text(drw, m->ww - sw, 0, sw, bh, 0, stext, 0);
+		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
+		drw_text(drw, m->ww - tw, 0, tw, bh, 0, stext, 0);
 	}
 
 	for (c = m->clients; c; c = c->next) {
@@ -749,7 +750,7 @@ drawbar(Monitor *m)
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
-	if ((w = m->ww - sw - x) > bh) {
+	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
@@ -1427,8 +1428,7 @@ run(void)
 
 void
 runAutostart(void) {
-	/* system("cd ~/.dwm; ./autostart_blocking.sh"); */
-	system("cd ~/.config/dwm; ./autostart.sh ");
+	system("cd ~/.config/dwm; ./autostart.sh");
 }
 
 void
@@ -1571,7 +1571,7 @@ setmfact(const Arg *arg)
 	if (!arg || !selmon->lt[selmon->sellt]->arrange)
 		return;
 	f = arg->f < 1.0 ? arg->f + selmon->mfact : arg->f - 1.0;
-	if (f < 0.1 || f > 0.9)
+	if (f < 0.05 || f > 0.95)
 		return;
 	selmon->mfact = f;
 	arrange(selmon);
@@ -1739,11 +1739,13 @@ tile(Monitor *m)
 		if (i < m->nmaster) {
 			h = (m->wh - my) / (MIN(n, m->nmaster) - i);
 			resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
-			my += HEIGHT(c);
+			if (my + HEIGHT(c) < m->wh)
+				my += HEIGHT(c);
 		} else {
 			h = (m->wh - ty) / (n - i);
 			resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0);
-			ty += HEIGHT(c);
+			if (ty + HEIGHT(c) < m->wh)
+				ty += HEIGHT(c);
 		}
 }
 
@@ -2192,20 +2194,22 @@ center(const Arg *unused)
 	             c->h);
 }
 
+
 void
 shiftview(const Arg *arg) {
-	Arg shifted;
+        Arg shifted;
 
-	if(arg->i > 0) // left circular shift
-		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
-		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+        if(arg->i > 0) // left circular shift
+                shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
+                   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
 
-	else // right circular shift
-		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
-		   | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+        else // right circular shift
+                shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
+                   | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
 
-	view(&shifted);
+        view(&shifted);
 }
+
 
 int
 main(int argc, char *argv[])
